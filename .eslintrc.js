1c6a24248ce460f42d6e2941c6c38ba66311e433
jbizima <jbizima@alustudent.com>
2024-07-24 22:42:24 +0200

    commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-      node: true,  // Add this line to recognize Node.js globals
-    },
-    extends: 'airbnb-base',
-    parserOptions: {
-      ecmaVersion: 12,
-      sourceType: 'module',
-    },
-    rules: {
-      // Add custom rules here
-    },
-  };
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,  // This line recognizes Node.js globals
+  },
+  extends: 'airbnb-base',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    // Add custom rules here
+  },
+};
   
