2f817fa438756391517c7169eb50b9b306a34c3d
jbizima <jbizima@alustudent.com>
2024-07-24 23:12:13 +0200

    commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,17 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import { defineConfig } from 'eslint';
 
-
-export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-];
+export default defineConfig({
+  env: {
+    node: true,   // Indicates that the code runs in a Node.js environment
+    es2021: true  // Enable ES2021 globals and syntax
+  },
+  extends: [
+    'airbnb-base' // Extend Airbnb base rules
+  ],
+  parserOptions: {
+    ecmaVersion: 2021, // Specify ECMAScript version
+  },
+  rules: {
+    // Custom rules or overrides
+  },
+});
